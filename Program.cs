ee129c3d176c3217a18a3af98368995bff190ab1
Marcelo Ungaretti <marcelo.ungaretti@gmail.com>
2021-07-02 14:07:02 -0300

    Fazendo os primeiros testes de movimentacao

--- Program.cs
+++ Program.cs
@@ -10,16 +10,25 @@ namespace xadrez_console
         {
             try
             {
-                var tab = new Tabuleiro(8, 8);
+                var partida = new PartidaDeXadrez();
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+                }
 
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
 
-                Tela.imprimirTabuleiro(tab);
+                
             }
             catch(TabuleiroException e)
             {
--- Tabuleiro/Peca.cs
+++ Tabuleiro/Peca.cs
@@ -17,6 +17,10 @@ namespace tabuleiro
             this.qteMovimentos = 0;
         }
 
+        public void incrementarQteMovimentos()
+        {
+            qteMovimentos++;
+        }
 
 
 
